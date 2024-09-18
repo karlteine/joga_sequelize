2aff46c625d0c6fd10c51a0047eb82e4ea800ec2
karl <karlkivisild@gmail.com>
2024-09-18 14:03:17 +0300

    Created article table and added table data

---
+++ seeders/20240918105156-demo-articles.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  up (queryInterface, Sequelize) {
+    return Promise.all([
+      queryInterface.bulkInsert('Articles', [{
+        name: 'Introduction to Ashtanga',
+        slug: 'introduction-to-ashtanga',
+        image: 'ashtanga.jpg',
+        body: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>',
+        published: '2020-01-08 15:02:30',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }]),
+      queryInterface.bulkInsert('Articles', [{
+        name: 'Morning vinyasa flow routine',
+        slug: 'morning-vinyasa-flow-routine',
+        image: 'morning.jpg',
+        body: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>',
+        published: '2020-04-14 15:02:41',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }]),
+      queryInterface.bulkInsert('Articles', [{
+        name: 'Secrets of a yoga teacher',
+        slug: 'secrets-of-a-yoga-teacher',
+        image: 'yoga-teacher.jpg',
+        body: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>',
+        published: '2020-05-28 15:02:55',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }]),
+    ])
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Articles', null, {})
+  }
+};

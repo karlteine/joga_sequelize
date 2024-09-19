25a55ebceb93b92e78af94602e565cef96a38fb7
karl <karlkivisild@gmail.com>
2024-09-19 08:49:28 +0300

    Getted author’s atricles data

--- controllers/article.js
+++ controllers/article.js
@@ -2,6 +2,7 @@ const Sequelize = require("sequelize")
 const sequelize = new Sequelize('mysql://root:qwerty@localhost:3306/joga_sequelize')
 
 const models = require('../models')
+const { Author, Article } = require('../models')
 
 const getAllArticles = (req, res) => {
   models.Article.findAll()
@@ -30,9 +31,32 @@ const getArticleBySlug = (req, res) => {
     .catch (error => {
         return res.status(500).send(error.message)
     })
-} 
+}
+
+const getArticlesByAuthor = async (req, res) => {
+    try {
+        const authorId = req.params.id;
+
+        const author = await Author.findByPk(authorId, {
+            include: {
+                model: Article,
+                as: 'articles'
+            }
+        });
+
+        if (!author) {
+            return res.status(404).json({ message: 'Author not found' });
+        }
+
+        res.json(author);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
 
 module.exports = {
     getAllArticles,
-    getArticleBySlug
+    getArticleBySlug,
+    getArticlesByAuthor
 } 
